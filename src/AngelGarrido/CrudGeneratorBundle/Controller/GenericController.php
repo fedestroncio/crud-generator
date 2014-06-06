cc10c83e8fbc104c9a541c950848db42374756a7
Angel Garrido <betunsmear@gmail.com>
2014-06-06 13:42:52 +0200

    [Controller][FIX] changed the create and delete actions to use the genericController.

--- src/AngelGarrido/CrudGeneratorBundle/Controller/GenericController.php
+++ src/AngelGarrido/CrudGeneratorBundle/Controller/GenericController.php
@@ -1,14 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Fede
- * Date: 6/06/14
- * Time: 12:50
- */
 
 namespace AngelGarrido\CrudGeneratorBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class GenericController {
+class GenericController extends Controller
+{
+    protected function flashSuccessCreate(Request $request, $entity)
+    {
+        $this->flash($request, 'success', 'flash.entity.create', $entity);
+    }
 
+    protected function flashErrorCreate(Request $request, $entity)
+    {
+        $this->flash($request, 'danger', 'flash.error.create', $entity);
+    }
+
+    protected function flashSuccessDelete(Request $request, $entity)
+    {
+        $this->flash($request, 'success', 'flash.entity.delete', $entity);
+    }
+
+    protected function flashErrorDelete(Request $request, $entity)
+    {
+        $this->flash($request, 'danger', 'flash.error.delete', $entity);
+    }
+
+    protected function flash(Request $request, $type, $action, $entity)
+    {
+        $request->getSession()->getFlashBag()->add($type, $this->translate($action, array('%entity%' => $entity)));
+    }
+
+    protected function translate($message, $params)
+    {
+        return $this->get('translator')->trans($message, $params, 'AngelGarridoCrudGeneratorBundle');
+    }
 } 
